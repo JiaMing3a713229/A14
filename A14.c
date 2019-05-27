11821ebeec9ae35a0dc357e58b94c0d93de59fee
JiaMing <ty10410205@gmail.com>
2019-05-27 20:05:02 +0800

    change .c

--- A14.c
+++ A14.c
@@ -1,15 +1,25 @@
+/*
+作業14:請撰寫一個程式，
+將字串str[]="I $am 5a$ go68od#@$ st-ude[nt]."
+中不是英文字母也不是空白的都刪掉，
+顯示出隱藏的一句話：I am a good student。
+*呂家明 3a713229
+*/ 
+ 
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+
 int main(void){
 	
-	int length;
+	int length;//字串長度變數 
 	int i;//loop
 	char input[]="I $am 5a$ go68od#@$ st-ude[nt].";
 	char *check;
 	
 	length=strlen(input);
 	check=input;
+	//檢查字串 
 	for(i=0;i<length;i++){
 		if(*(check+i)<122&&*(check+i)>=96){
 			printf("%c", *(check+i));
@@ -21,6 +31,7 @@ int main(void){
 			printf("%c", *(check+i));
 		}
 	}
+	printf("\n\n");
 	system("pause");
 	return 0;
 } 
